4ab39107443fe0443ca3031ed585b422e525d905
mmmlm2570-wq <mmmlm2570@gmail.com>
2026-09-12 15:30:45 +0300

    Add file management Odoo 19 module: __manifest__.py

---
+++ file_management/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'إدارة الملفات',
+    'summary': 'إدارة بسيطة للملفات والمرفقات',
+    'version': '19.0.1.0.0',
+    'category': 'Productivity',
+    'author': 'Your Company',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/file_management_security.xml',
+        'security/ir.model.access.csv',
+        'data/file_category_data.xml',
+        'views/file_category_views.xml',
+        'views/file_document_views.xml',
+        'views/file_management_menus.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
+
