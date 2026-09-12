c776ed054394a33457ab5ebae2410dd30d874413
mmmlm2570-wq <mmmlm2570@gmail.com>
2026-09-12 15:30:55 +0300

    Add file management Odoo 19 module: models/file_category.py

---
+++ file_management/models/file_category.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+
+class FileManagementCategory(models.Model):
+    _name = 'file.management.category'
+    _description = 'File Category'
+    _order = 'name'
+
+    name = fields.Char(string='اسم التصنيف', required=True, translate=True)
+    active = fields.Boolean(default=True)
+
+    _sql_constraints = [
+        ('file_management_category_name_unique', 'unique(name)', 'The category name must be unique.'),
+    ]
+
